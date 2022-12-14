5ce1181b1bf9243c9c31a511b0ebbb4aa748f236
Abdourahmane <abdourahmane@localhost.localdomain>
2022-12-14 12:08:40 +0100

    deploy

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ app.use(cors({origin: '*'}));
 app.use(function (req, res, next) {
     //Enabling CORS
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", " http://localhost:4200");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization");
       next();
 });
 app.use(express.json());

b3eb8d04ca92155b5a07462298a8f3c1d07b5c23
Abdourahmane <abdourahmane@localhost.localdomain>
2022-12-14 12:04:32 +0100

    deploy

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ const database = mongoose.connection
 
 const app = express();
 app.use(cors({origin: '*'}));
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+});
 app.use(express.json());
 
 //app.use(bodyParser.urlencoded({ extended: false }))
